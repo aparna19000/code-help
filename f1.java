b644684511ad8956622cc1caf8e4cf2215ad56d7
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2024-07-03 19:37:02 +0530

    Create f1.java

---
+++ f1.java
@@ -0,0 +1,12 @@
+class Human{
+    private int age;
+    private  String name;
+}
+public class HelloWorld {
+    public static void main(String[] args) {
+     Human obj = new Human();
+     obj.age = 11;
+     obj.name = "Aparna";
+     System.out.println(obj.name);
+    }
+}
