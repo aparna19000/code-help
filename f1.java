cac7250fc4a1a364121bf38ce2ee89213496928b
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2024-07-03 19:40:47 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -1,4 +1,20 @@
 class Human{
+    int age ;
+    String name;
+}
+   
+public class HelloWorld {
+    public static void main(String[] args) {
+     Human obj = new Human();
+     obj.age = 11;
+     obj.name = "Aprana";
+     
+     System.out.println(obj.name);
+    }
+}
+----------------------------------------------------------
+
+class Human{
     private int age;
     private  String name;
 }
