9e8c92bd8e02cee810d7b4e023797bcb070683dc
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2024-07-03 19:50:24 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -26,3 +26,32 @@ public class HelloWorld {
      System.out.println(obj.name);
     }
 }
+Every time u create a instance variable make it private, nobody should be able to use it, so u might think, if noone is able to access the data, 
+then y are we making it. I mean we make it for people to access them, but not directly. 
+    We should be able to use by methods, by behavour
+
+class Human{
+    private int age =11;
+    private String name = "Aparna";
+    
+    public int  getAge()
+    {
+        return age;
+    }
+    public String getName()
+    {
+        return name;
+    }
+}
+   
+public class HelloWorld {
+    public static void main(String[] args) {
+     Human obj = new Human();
+     //obj.age = 11;
+     //obj.name = "Aprana";
+    
+     System.out.println(obj.getAge());
+     System.out.println(obj.getName());
+    }
+}
+    
